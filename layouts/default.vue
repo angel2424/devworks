9c08f706ca5321ba5bc992bae7d31978f765d872
Angel Rodriguez <angel.rodriguez@cimagroup.com.mx>
2025-04-21 17:55:44 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <VueLenis root>
+    <div>
+      <slot />
+    </div>
+  </VueLenis>
+</template>
+
+<script setup>
+import { VueLenis, useLenis } from 'lenis/vue'
+import { watch } from 'vue'
+
+const lenis = useLenis(() => {
+})
+
+watch(lenis, () => {
+
+})
+</script>
